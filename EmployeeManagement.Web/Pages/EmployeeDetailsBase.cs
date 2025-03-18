ae76528ad2f0f28a16f9e139f2e278350d304dfa
JaredKishCodes <missionajk@gmail.com>
2025-03-18 18:07:18 +0800

    added new features

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeManagement.Web.Services;
 using EmployeeManagements.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using System;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Web.Pages
@@ -14,11 +17,31 @@ namespace EmployeeManagement.Web.Pages
         [Parameter]
         public string Id { get; set; }
 
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected bool FooterVisible { get; set; } = true;
+
         protected override async Task OnInitializedAsync()
         {
             Id = Id ?? "1";
           Employee = await EmployeeService.GetEmployeeById(int.Parse(Id));
         }
+
+        protected async Task Button_Click()
+        {
+            Console.WriteLine("Button Clicked!"); // Debugging Log
+
+
+            FooterVisible = !FooterVisible;
+            ButtonText = FooterVisible ? "Hide Footer" : "Show Footer";
+                
+            
+            
+           
+            await InvokeAsync(StateHasChanged); // Ensures UI update
+        }
+
 
     }
 }
